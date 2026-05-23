a3225b56c2bd42aaae5d25a44da130cf3a5d270f
81dee <deepaksingh2004123@gmail.com>
2026-05-24 01:13:29 +0530

    update port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: "https://job-portal-backend-g2rd.onrender.com",
+    baseURL: "http://localhost:3000/api",
 });
 
 // attach token in every request
